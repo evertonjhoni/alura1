b4d486e6bec0fe87c23c8127f1de2e8f501ec4e0
evertonjhoni <everton.albuquerque@escola.pr.gov.br>
2024-11-14 11:03:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,95 @@
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+function criaCartao (categoria, pergunta, resposta) {
+
+}
+function criaCartao (categoria, pergunta, resposta) {
+    console.log(categoria, pergunta, resposta)
+}
+function criaCartao(categoria, pergunta, resposta) {
+    let container 
+}
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+}
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+}
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+}
+container.appendChild(cartao)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
+let container = document.getElementById('container')
+let cartao = document.createElement('article')
+container.appendChild(cartao)
+let respostaEstaVisivel = false
+function viraCartao(){
+    respostaestaVisivel = !respostaEstaVisivel
+}
+function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+cartao.addEventListener('click', viraCartao)
